55220247db6d5ec0261c246dbf8431a3d6255eac
Krzysztof Lubiniecki <klubiniecki@gmail.com>
2019-05-22 08:25:56 +0200

    feat(cors): add cors

--- api/index.ts
+++ api/index.ts
@@ -2,17 +2,20 @@ import { GraphQLSchema } from "graphql";
 import * as express from "express";
 import * as graphqlHTTP from "express-graphql";
 import * as dotenv from "dotenv";
+import * as cors from "cors";
 import initDB from "./db";
 import query from "./graphql/queries";
 import mutation from "./graphql/mutations";
 
 dotenv.config();
+
 initDB();
 
 const app = express();
 app.on("error", err => {
   console.log("server error", err);
 });
+app.use(cors());
 
 const schema = new GraphQLSchema({ query, mutation });
 app.use(
